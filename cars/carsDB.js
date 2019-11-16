881ae83d5689491cead6e3cff3cbd2b8dcfbc50c
AustinLynes <austinlynes94@gmail.com>
2019-11-15 19:11:12 -0800

    added some more crud that i ddint realize i didnt do

--- cars/carsDB.js
+++ cars/carsDB.js
@@ -5,15 +5,28 @@ function find() {
     return db('cars')
 }
 function findById(id) {
-
+    return db('cars').where('id', id).first()
 }
 function insert(car) {
     return db('cars')
         .insert(car)
         .then(ids => ({ id: ids[0] }))
 }
+
+function remove(id){
+    return db('cars')
+        .where('id', id)
+        .del()
+}
+function update(id, changes){
+    return db('cars')
+        .update(changes)
+        .where('id', id)
+}
 module.exports = {
     find,
     findById,
-    insert
+    insert,
+    remove,
+    update
 }
--- cars/carsRoute.js
+++ cars/carsRoute.js
@@ -4,24 +4,71 @@ const Cars = require('./carsDB')
 //GET
 router.get('/', (req, res) => {
     Cars.find()
-    .then(car=>{
-        res.json(car)
-    }).catch(()=>{
-        res.status(404).json({messege:'cars not found'})
-    })
+        .then(car => {
+            res.json(car)
+        }).catch(() => {
+            res.status(404).json({ messege: 'cars not found' })
+        })
 })
 //POST
-router.post('/', (req, res)=>{
+router.post('/', (req, res) => {
     const car = req.body
-    if(!car){
-        res.status(400).json({message:'car information not found'})
-    }else{
-        Cars.insert(car).then(()=>{
-            res.status(201).json({message:'car created successfully '})
-        }).catch(()=>{
-            res.status(500).json({message:'car is missing some required data'})
+    if (!car) {
+        res.status(400).json({ message: 'car information not found' })
+    } else {
+        Cars.insert(car).then(() => {
+            res.status(201).json({ message: 'car created successfully ' })
+        }).catch(() => {
+            res.status(500).json({message: 'uh oh.... '  })
         })
     }
+})
+//getby
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    Cars.findById(id)
+        .then((_car) => {
+            if (!_car) {
+                res.status(400).json({ message: 'car information not found' })
+            } else {
+
+                res.status(200).json(_car)
+            }
+        }).catch(() => {
+            res.status(500).json({ message: 'uh oh.... '  })
+        })
+}
+)
+//delete
+router.delete('/:id', (req, res) => {
+    const {id} = req.params
+    Cars.remove(id)
+        .then((_car) => {
+            if (!_car) {
+                res.status(400).json({ message: 'car information not found' })
+            } else {
+
+                res.status(200).json({ message: 'car removed success',_car})
+            }
+        }).catch(() => {
+            res.status(500).json({ message: 'uh oh.... ' })
+        })
+})
+//update
+router.put('/', (req, res) => {
+    const {id} = req.params
+    const {changes} = req.body
+    Cars.update(id, changes)
+        .then((_car) => {
+            if (!_car) {
+                res.status(400).json({ message: 'car information not found' })
+            } else {
+
+                res.status(200).json({ message: 'car updated success',_car})
+            }
+        }).catch(() => {
+            res.status(500).json({ message: 'uh oh.... ' })
+        })
 })
 
 module.exports = router
